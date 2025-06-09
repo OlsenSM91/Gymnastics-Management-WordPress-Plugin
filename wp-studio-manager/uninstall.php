68356fc99a003e07b9e03ffb0d4088a00488a979
Steven Olsen <130707762+OlsenSM91@users.noreply.github.com>
2025-06-08 20:09:51 -0700

    Implement database setup

--- wp-studio-manager/uninstall.php
+++ wp-studio-manager/uninstall.php
@@ -1,1 +1,10 @@
-<?php\n// Uninstall hook
+<?php
+// Ensure WordPress is uninstalling
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit();
+}
+
+require_once plugin_dir_path(__FILE__) . 'core/class-wsm-loader.php';
+WSM\Core\WSM_Loader::register();
+
+WSM\Core\WSM_Database::uninstall();
